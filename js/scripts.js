195c3ec0a013e6e4eeb512d7592efcd64e522886
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-27 10:07:34 -0700

    set up for the master merge

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,72 @@ let newGame = new ScoreCard(player1, player2);
 let attachEventListeners = function () {
   // Event Listener for Player 1 Roll
   $("button#player1Roll").click(function () {
+    let roll = player1.rollDice();
+    $("span#roll").text(roll);
+    $("span#player1TurnScore").text(player1.turnScore);
+
+
+
+
+    // If player one, rolls a one, need to disable buttons
+    if (roll === 1) {
+      // Disable player one's buttons
+      $("button.player1Btn").prop("disabled", true);
+      // Enable player two's buttons
+      $("button.player2Btn").prop("disabled", false);
+      newGame.currentPlayer = 2;
+    };
+  });
+
+  // Event Listener for Player 1 Hold
+  $("button#player1Hold").click(function () {
+    let score = player1.hold();
+    $("span#player1TotalScore").text(score);
+    $("span#player1TurnScore").text(0);
+    // Disable player one's buttons
+    $("button.player1Btn").prop("disabled", true);
+    // Enable player two's buttons
+    $("button.player2Btn").prop("disabled", false);
+    // Change current player
+    newGame.currentPlayer = 2;
+
+  });
+
+  //  Event Listener for Player 2 Roll
+  $("button#player2Roll").click(function () {
+    let roll = player2.rollDice();
+    $("span#roll").text(roll);
+    $("span#player2TurnScore").text(player2.turnScore);
+
+    // If player two, rolls a one, need to disable buttons
+    if (roll === 1) {
+      // Disable player two's buttons
+      $("button.player2Btn").prop("disabled", true);
+      // Enable player one's buttons
+      $("button.player1Btn").prop("disabled", false);
+      newGame.currentPlayer = 1;
+    };
+  });
+
+  // Event Listener for Player 2 Hold
+  $("button#player2Hold").click(function () {
+    let score = player2.hold();
+    $("span#player2TotalScore").text(score);
+    $("span#player2TurnScore").text(0);
+    // Disable player two's buttons
+    $("button.player2Btn").prop("disabled", true);
+    // Enable player one's buttons
+    $("button.player1Btn").prop("disabled", false);
+    // Change current player
+    newGame.currentPlayer = 1;
+  });
+
+};
+
+
+let attachEventListeners2 = function () {
+  // Event Listener for Player 1 Roll
+  $("button#player1Roll").click(function () {
     let roll1 = player1.rollDice();
     let roll2 = player1.rollDice();
     $("span#roll1").text(roll1);
